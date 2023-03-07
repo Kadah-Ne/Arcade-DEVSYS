99339f2f5e4b1eb5cf01d66d6a47e2100f89d925
Kadah-Ne <martin.gouviaux@epsi.fr>
2023-03-07 10:46:28 +0100

    c'mon anthony

--- brickbreaker/pongchilling/urls.py
+++ brickbreaker/pongchilling/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Login, name='Login'),
+    path('index',views.Index,name = "Index"),
 ]
--- brickbreaker/pongchilling/views.py
+++ brickbreaker/pongchilling/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Login(request):
     return render(request, 'Login.html')
+def Index(request):
+    return render(request,"index.html")
