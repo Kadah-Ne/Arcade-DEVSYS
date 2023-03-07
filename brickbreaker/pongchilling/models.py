78e2ac096dba52db0c0f559cf539fb09f64cf9f8
Whitecrust <nizarsoufiya@gmail.com>
2023-03-07 10:57:18 +0100

    model done

--- brickbreaker/pongchilling/models.py
+++ brickbreaker/pongchilling/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length=255,null=True)
+    mdp=models.CharField(max_length=255,null=True)
+    currentscore=models.IntegerField()
+    topscore=models.IntegerField()
+    level=models.IntegerField()
+    def __str__(self):
+        return self.username,self.mdp,str(self.score)
+
+def searchSave(TargetUsername):
+    user = User.objects.filter(username = TargetUsername)
+    if user:
+        return str(user[0].split(",")[1])
+    else:
+        return None
+def createUser(NewUsername,NewMdp):
+    newUser = User(username=NewUsername,mdp = NewMdp)
+    newUser.save()
+
+def saveLevel(request,levelid):
+    TargetUsername= request.session.get("userName")
+    user = User.objects.filter(username=TargetUsername)[0]
+    user.level = levelid
+    user.save()
+
+def saveScore(request,score):
+    TargetUsername= request.session.get("userName")
+    user = User.objects.filter(username=TargetUsername)[0]
+    user.currentscore = score
+
+    if user.topscore < score:
+        user.topscore = score
+
+    user.save()
