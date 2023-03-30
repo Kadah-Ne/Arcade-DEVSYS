16e71f675e00566f20ef0614f3c8f11082e91bf4
Saulight73 <ninimeme73@gmail.com>
2023-03-30 15:58:10 +0200

    push

--- brickbreaker/pongchilling/tests.py
+++ brickbreaker/pongchilling/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from unittest import TestCase
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# Create your tests here.
+
+
+
+class testTestwithnewaccount(TestCase):
+  def setUp(self):
+    self.driver = webdriver.Firefox()
+    self.vars = {}
+  
+  
+  
+  def test_testwithnewaccount(self):
+    self.driver.get("http://127.0.0.1:8000/pongchilling/")
+    self.driver.set_window_size(970, 1032)
+    self.driver.find_element(By.NAME, "userName").click()
+    self.driver.find_element(By.NAME, "userName").send_keys("nicolas12")
+    self.driver.find_element(By.NAME, "password").click()
+    self.driver.find_element(By.NAME, "password").send_keys("nicolas")
+    self.driver.find_element(By.CSS_SELECTOR, ".brickValidation").click()
+    self.driver.find_element(By.NAME, "oui").click()
+
+
+  def tearDown(self):
+    self.driver.quit()
+
+# a = TestTestwithnewaccount()
+# a.setup_method()
+# a.test_testwithnewaccount()
+# a.teardown_method()
